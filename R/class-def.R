61061caad30a0e985b9996ae137e767f76171a0f
muscle <1059779096@qq.com>
2017-08-07 23:14:37 +0800

    dev 9000

--- R/class-def.R
+++ R/class-def.R
@@ -1,6 +1,10 @@
-#' An S4 class to represent a trade data set producted by quant model
+#' An S4 class for quant strategy judgements.
 #'
 #' @slot trade.info A data.frame
+#' @slot data.level A character value of one of c('1day', '60mins', '1min')
+#' @slot bt.features A list contains severial features about quant strategy
+#'   judegments produced by the back-test process.
+#' @export
 quantler <- setClass('quantler',
                      slots = list(trade.info  = 'data.frame',
                                   data.level  = 'character',
@@ -8,3 +12,16 @@ quantler <- setClass('quantler',
                                   ),
                      contains = 'list')
 
+
+#' Is an object a quantler object?
+#'
+#' Checks for quantler objects.
+#'
+#' @param object An object.
+#'
+#' @return A logical value. TRUE when the input is a quantler object and FALSE
+#'   otherwise.
+#' @export
+is.quantler <- function(object){
+  # inherits(object, 'quantler')
+}
--- R/class-quantler-method.R
+++ R/class-quantler-method.R
@@ -1,23 +1,6 @@
 #' @include class-def.R
 NULL
 
-
-
-#' Is an object a quantler object?
-#'
-#' Checks for quantler objects.
-#'
-#' @param object An object.
-#'
-#' @return A logical value. TRUE when the input is a quantler object and FALSE
-#'   otherwise.
-#' @describeIn quantler if an object is a quantler object
-#' @export
-is.quantler <- function(object){
-  inherits(object, 'quantler')
-}
-
-
 #' Conversion functions to coerce data objects of arbitrary classes to class
 #' quantler and back, without losing any attributes of the original format.
 setGeneric('as.quantler', function(object){
